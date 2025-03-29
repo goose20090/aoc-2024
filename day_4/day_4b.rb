b995e8c01dd9c0e216a3f848b1d9332dd5987ca8
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-03-29 10:11:51 +0000

    final solution

--- day_4/day_4b.rb
+++ day_4/day_4b.rb
@@ -1,83 +1,72 @@
 require 'pry'
 str = File.read(File.join(__dir__, 'input.txt'))
 
-# split string into matrix
-
 grid = str.split(/\n/).map(&:chars)
 
-def count_xmas(grid)
+
+def check_mas_pattern(grid, row, col, diagonal_coords)
+  result = ""
+  diagonal_coords.each do |co_ordinates|
+    x, y = co_ordinates
+    result += grid[row + x][col + y]
+  end
+  result == "MAS" || result == "SAM"
+end
+
+def valid_x_positions?(diagonals, row, col, height, width)
+  diagonals.flatten(1).each do |co_ordinate|
+    next if co_ordinate == [0, 0]
+
+    x, y = co_ordinate
+    result_x = row + x
+    result_y = col + y
+
+    return false if result_x < 0 || result_x >= height
+    return false if result_y < 0 || result_y >= width
+  end
+  true
+end
+
+def count_x_mas(grid)
   height = grid.length
+  return 0 if height == 0
   width = grid[0].length
+  return 0 if width == 0
+
   count = 0
 
-  directions = [
-    [0, 1],   # right
-    [1, 0],   # down
-    [1, 1],   # down-right
-    [-1, 1],  # up-right
-    [0, -1],  # left
-    [-1, 0],  # up
-    [-1, -1], # up-left
-    [1, -1]   # down-left
-  ]
-
-
-  top_left_to_bottom_right = [ 
+  top_left_to_bottom_right = [
     [-1, -1],
     [0, 0],
-    [1,1],
+    [1, 1]
   ]
 
   top_right_to_bottom_left = [
-    [1,1],
-    [0,0],
-    [1,1]
+    [-1, 1],
+    [0, 0],
+    [1, -1]
   ]
 
+  diagonals = [top_left_to_bottom_right, top_right_to_bottom_left]
 
-  cross = [top_left_to_bottom_right, top_right_to_bottom_left]
-
-
-(0...height).each do |row|
+  (0...height).each do |row|
     (0...width).each do |col|
       next unless grid[row][col] == "A"
-      direction_1, direction_2 = cross
-      next unless valid_position?(cross, row, col, height, width)
-      if check_xmas(grid, row, col, direction_1) && check_xmas(grid, row, col, direction_2)
-          count += 1
+
+      diag1_coords, diag2_coords = diagonals
+
+      next unless valid_x_positions?(diagonals, row, col, height, width)
+
+      if check_mas_pattern(grid, row, col, diag1_coords) && \
+         check_mas_pattern(grid, row, col, diag2_coords)
+        count += 1
       end
     end
   end
   count
 end
 
+result = count_x_mas(grid)
 
-def valid_position?(cross, row, col, height, width)
-  cross.flatten(1).each do |co_ordinate|
-    next if co_ordinate == [0,0]
-    x, y = co_ordinate
+puts "X-MAS appears #{result} times in the word search"
 
-    result_x = row + x
-    result_y = col + y
-
-    return false if result_x < 0 || result_x >= height
-    return false if result_y < 0 || result_y >= height
-  end
-  true
-end
-
-
-def check_xmas(grid, row, col, cross_cordinates)
-  result = ""
-  cross_cordinates.each do |co_ordinates|
-    x, y = co_ordinates
-
-    result += grid[row + x][col + y]
-  end
-  binding.pry
-  result == "MAS" || result == "SAM"
-end
-
-result = count_xmas(grid)
-
-puts "XMAS appears #{result} times in the word search"
